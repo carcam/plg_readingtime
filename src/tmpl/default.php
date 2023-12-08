9254e3ef83547eac593e223d3c9d3cf2e02ab879
Carlos Cámara <carlos@hepta.es>
2023-12-08 06:49:59 +0100

    Fixed folder structure and gulp scripts

---
+++ src/tmpl/default.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author    Carlos M. Cámara <carlos@hepta.es>
+ * @copyright 2012-2016 Hepta Technologies SL. All rights reserved.
+ * @url       https://extensions.hepta.es
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ **/
+
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die;
+
+if ($quickTime !== $slowTime) {
+    $formattedTime = $quickTime . " - " . $slowTime;
+} else {
+    $formattedTime = $slowTime;
+}
+?>
+
+<div class="reading-time">
+    (<?php echo Text::_('PLG_READINGTIME_LABEL'); ?>:
+        <?php echo $formattedTime
+        . " " .
+        Text::plural('PLG_READINGTIME_N_MINUTES', $slowTime) ?>)
+</div>
